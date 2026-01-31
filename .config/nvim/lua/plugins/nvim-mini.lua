854789a09c09ef8f95502137ea586e2142d4316a
Baba Tova <BabaTova80@proton.me>
2026-01-31 19:16:42 +0200

    plugins change

--- .config/nvim/lua/plugins/nvim-mini.lua
+++ .config/nvim/lua/plugins/nvim-mini.lua
@@ -6,12 +6,12 @@
 -- ================================================================================================
 
 return {
-	{ "echasnovski/mini.ai", version = "*", opts = {} },
+	-- { "echasnovski/mini.ai", version = "*", opts = {} },
 	{ "echasnovski/mini.comment", version = "*", opts = {} },
 	-- { "echasnovski/mini.cursorword", version = "*", opts = {} },
 	{ "echasnovski/mini.move", version = "*", opts = {} },
 	{ "echasnovski/mini.surround", version = "*", opts = {} },
 	{ "echasnovski/mini.indentscope", version = "*", opts = { style = "|" } },
-	{ "echasnovski/mini.pairs", version = "*", opts = {} },
+	-- { "echasnovski/mini.pairs", version = "*", opts = {} },
 	{ "echasnovski/mini.notify", version = "*", opts = {} },
 }
