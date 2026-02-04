5a5acaaaf9c851e3da073bab87bef07ce5da0269
Baba Tova <BabaTova80@proton.me>
2026-02-04 23:16:43 +0200

    cooler blame

--- .config/nvim/lua/plugins/git-blame-nvim.lua
+++ .config/nvim/lua/plugins/git-blame-nvim.lua
@@ -7,7 +7,6 @@
 
 return {
 	"f-person/git-blame.nvim",
-	-- load the plugin at startup
 	event = "VeryLazy",
 	-- Because of the keys part, you will be lazy loading this plugin.
 	-- The plugin will only load once one of the keys is used.
@@ -17,8 +16,8 @@ return {
 		-- your configuration comes here
 		-- for example
 		enabled = true, -- if you want to enable the plugin
-		message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
-		date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+		message_template = " <summary> • <date> • <author>", -- template for the blame message, check the Message template section for more options
+		date_format = "%r", -- template for the date, check Date format section for more options
 		virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
 	},
 }
